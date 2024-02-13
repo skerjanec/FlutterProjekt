d460c40f6fdd9a36334061530a8aeb269aadb3eb
skerjanec <nika.skerjanec1235@gmail.com>
2024-02-13 17:06:41 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Perform login operation here
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+                print('Username: $username');
+                print('Password: $password');
+              },
+              child: Text('Login'),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginPage(),
+  ));
 }
