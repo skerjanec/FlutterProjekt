8bd4e110f4d9aa79ae751441b1babc23da1e616a
skerjanec <nika.skerjanec1235@gmail.com>
2024-03-15 12:05:27 +0100

    calendar fixes

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -9,15 +9,22 @@ import 'package:intl/date_symbol_data_local.dart';
 
 
 /// KOLEDAR!!!
-
 class Event {
   final String title;
+  final DateTime time;
 
-  const Event(this.title);
+  const Event(this.title, this.time);
 
   @override
-  String toString() => title;
+  String toString() => '$title - ${_formatTime(time)}';
+
+  String _formatTime(DateTime time) {
+    String hour = time.hour.toString().padLeft(2, '0'); // Ensure two digits with leading zeros
+    String minute = time.minute.toString().padLeft(2, '0'); // Ensure two digits with leading zeros
+    return '$hour:$minute';  
+  }
 }
+
 
 class CustomTableCalendar extends StatefulWidget {
   @override
@@ -25,8 +32,8 @@ class CustomTableCalendar extends StatefulWidget {
 }
 
 class _CustomTableCalendarState extends State<CustomTableCalendar> {
-  DateTime? _selectedDay;
-  DateTime? _focusedDay;
+  late DateTime _selectedDay;
+  late DateTime _focusedDay;
   CalendarFormat _calendarFormat= CalendarFormat.week;
   late Map<DateTime, List> _eventsList = {};
 
@@ -37,7 +44,7 @@ class _CustomTableCalendarState extends State<CustomTableCalendar> {
   void readEventsDatabase(){
     print("READ DATABASE_______________________");
     DateTime d = DateTime.now();
-    DateTime date = DateTime(d.year,d.month,d.day);
+    DateTime date = DateTime(d.year,d.month,d.day,d.hour,d.minute);
     _eventsList[date.subtract(Duration(days: 2))] = ['Event A1', 'Event B1'];
     _eventsList[date] = ['Event E1', 'Event R1'];
   }
@@ -49,6 +56,7 @@ class _CustomTableCalendarState extends State<CustomTableCalendar> {
     // TO DO - read from database to add events k se bojo dodal na koledar
     // pod team - Sezona - Termini
     // pomoje more pogledat, za koga kaže to če se nrdi za igralce
+    _focusedDay = DateTime.now();
     _selectedDay = _focusedDay;
     readEventsDatabase();
   }
@@ -62,28 +70,53 @@ class _CustomTableCalendarState extends State<CustomTableCalendar> {
     )..addAll(_eventsList);
 
     List getEventForDay(DateTime day) {
-      return _events[day] ?? [];
+      print("GET EVDENT FOR DAY :::::::::::::::::::::::::::::::::");
+      return _eventsList.entries
+          .where((entry) =>
+              entry.key.year == day.year &&
+              entry.key.month == day.month &&
+              entry.key.day == day.day)
+          .expand((entry) => entry.value.map((e)=>Event(e, entry.key)))
+          .toList();
     }
 
     // TO DO - add - to database 
     void addEvent(DateTime day, bool weekly, String name){
       print("ADD EVENT FUNCTION________________________________________");
-      print(_eventsList[day]);
-      if (_eventsList == null || _eventsList[day] == null || _eventsList[day]!.isEmpty) _eventsList[day] = [name];
-      else _eventsList[day]!.add(name); // ! je tm če null, ??
-      print(_eventsList[day]);
-      this.build(context);
+      if(weekly){
+          //TO DO - event vpiše do konca sezone isti dan 
+          // spremeni isBEFORE da ma - end date
+        DateTime eventDate = day;
+        for (int i = 0; i < 52 || eventDate.isBefore(DateTime.now().add(Duration(days: 30))); i++) {
+          eventDate = day.add(Duration(days: i * 7));
+          if (_eventsList == null || _eventsList[eventDate] == null || _eventsList[eventDate]!.isEmpty) _eventsList[eventDate] = [name];
+          else _eventsList[eventDate]!.add(name);
+        }
+      }
+      else{
+        print(_eventsList[day]);
+        if (_eventsList == null || _eventsList[day] == null || _eventsList[day]!.isEmpty) _eventsList[day] = [name];
+        else _eventsList[day]!.add(name); // ! je tm če null, ??
+        print(_eventsList[day]);
+      }
     }
     // TO DO - remove from database
     void removeEvent(DateTime day, String name){
 
+    }
+
+    void updateCalendar(DateTime day){
+      setState(() {
+        _selectedDay = day;
+        _focusedDay = day;
+      });
     }
     return Column(
         children: [
           TableCalendar(
             firstDay: DateTime.utc(2021, 1, 1),
             lastDay: DateTime.utc(2050, 12, 31),
-            focusedDay: DateTime.now(),
+            focusedDay: _focusedDay,
             eventLoader: getEventForDay,
             calendarFormat: _calendarFormat,
             onFormatChanged: (format) {
@@ -122,25 +155,30 @@ class _CustomTableCalendarState extends State<CustomTableCalendar> {
               // Dialog -> for adding new event
             TextEditingController _nameController = TextEditingController();
 
-              DateTime? dayChosen;
+
+
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                // Define a boolean variable to track the checkbox state
+              DateTime? dayChosen = _selectedDay;
               bool weeklyRepeat = false;
               String eventName = "test";
 
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
+                return StatefulBuilder(
+                  builder: (BuildContext context, StateSetter setState) {
                   return AlertDialog(
                     title: Text('Dodaj nov dogodek'),
-                    content: Text('Here you can add event details.'),
+                    content: Text('Ustvari nov dogodek.'),
                     actions: [
                       DateTimeFormField(
-                        mode: DateTimeFieldPickerMode.date,
+                        mode: DateTimeFieldPickerMode.dateAndTime,
                         decoration: const InputDecoration(
                           labelText: 'Datum dogodka',
                         ),
-                        firstDate: DateTime.now().subtract(const Duration(days: 10)),
-                        lastDate: DateTime.now().add(const Duration(days: 40)),
-                        initialPickerDateTime: DateTime.now().add(const Duration(days: 20)),
+                        firstDate: DateTime.now().subtract(const Duration(days: 365)),
+                        lastDate: DateTime.now().add(const Duration(days: 365)),
+                        initialPickerDateTime: _selectedDay,
                         onChanged: (DateTime? value) {
                           dayChosen = value;
                         },
@@ -149,7 +187,9 @@ class _CustomTableCalendarState extends State<CustomTableCalendar> {
                         title: Text('se ponavlja vsak teden'),
                         value: weeklyRepeat,
                         onChanged: (bool? value) {
+                          print("on changedddddddddddddddddd");
                           setState(() {
+                            print("set stateeeeeeeeeeeeeee");
                             weeklyRepeat = value!;
                           });
                         },
@@ -157,26 +197,25 @@ class _CustomTableCalendarState extends State<CustomTableCalendar> {
                       TextField(
                         controller: _nameController,
                         decoration: InputDecoration(
-                          labelText: 'Username',
+                          labelText: 'Ime dogodka',
                         ),
                       ),
                       TextButton(
                         onPressed: () {
                           eventName = _nameController.text;
                           print("ADDDD EVENTTTTTTTT");
-                          print(dayChosen);
-                          print(weeklyRepeat);
-                          print(eventName);
-                          print(_eventsList);
                           addEvent(dayChosen!, weeklyRepeat, eventName);
                           Navigator.of(context).pop();
+                          updateCalendar(dayChosen!);
                         },
                         child: Text('add event'),
                       ),
                     ],
                   );
-                },
-              );
+                  }
+                );
+              }
+            );
             },
             child: Text('+'),
             style: ElevatedButton.styleFrom(
@@ -210,7 +249,7 @@ class _CalendarPageState extends State<CalendarPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pozabljeno geslo'),
+        title: Text('Termini'),
       ),
       body: Padding(
         padding: EdgeInsets.all(50.0),
@@ -222,9 +261,9 @@ class _CalendarPageState extends State<CalendarPage> {
               decoration: const InputDecoration(
                 labelText: 'Datum začetka sezone',
               ),
-              firstDate: DateTime.now().add(const Duration(days: 10)),
-              lastDate: DateTime.now().add(const Duration(days: 40)),
-              initialPickerDateTime: DateTime.now().add(const Duration(days: 20)),
+              firstDate: DateTime.now().subtract(const Duration(days: 365)),
+              lastDate: DateTime.now().add(const Duration(days: 365)),
+              initialPickerDateTime: DateTime.now(),
               onChanged: (DateTime? value) {
                 _selectedDateStart = value;
               },
@@ -235,9 +274,9 @@ class _CalendarPageState extends State<CalendarPage> {
               decoration: const InputDecoration(
                 labelText: 'Datum konca sezone',
               ),
-              firstDate: DateTime.now().add(const Duration(days: 10)),
-              lastDate: DateTime.now().add(const Duration(days: 40)),
-              initialPickerDateTime: DateTime.now().add(const Duration(days: 20)),
+              firstDate: DateTime.now().subtract(const Duration(days: 365)),
+              lastDate: DateTime.now().add(const Duration(days: 365)),
+              initialPickerDateTime: DateTime.now(),
               onChanged: (DateTime? value) {
                 _selectedDateEnd = value;
               },
